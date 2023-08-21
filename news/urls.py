e0c34abe28838234f517eed2a40f65fcb582756f
Vitor Wagner <vitormwagner@gmail.com>
2023-08-21 17:02:57 -0300

    Feat: :sparkles: Added news details page

--- news/urls.py
+++ news/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from news.views import home
+from news.views import home, news_details
 
 urlpatterns = [
     path("", home, name="home-page"),
+    path("news/<int:id>", news_details, name="news-details-page")
 ]
--- news/views.py
+++ news/views.py
@@ -6,3 +6,9 @@ def home(request):
     news = News.objects.all()
     context = {"news": news}
     return render(request, "home.html", context)
+
+
+def news_details(request, id):
+    news = News.objects.get(id=id)
+    context = {"news": news}
+    return render(request, "news_details.html", context)
