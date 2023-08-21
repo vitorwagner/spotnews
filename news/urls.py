e4d9087408a6ff97b1ef60e553cde6e11f04ac93
Vitor Wagner <vitormwagner@gmail.com>
2023-08-21 19:00:27 -0300

    Feat: :sparkles: Added users DRF route

--- news/urls.py
+++ news/urls.py
@@ -2,9 +2,11 @@ from django.urls import path, include
 from news.views import home, news_details, category, news_form
 from rest_framework import routers
 from news_rest.views.categories_view import CategoriesViewSet
+from news_rest.views.users_view import UsersViewSet
 
 router = routers.DefaultRouter()
 router.register(r"categories", CategoriesViewSet)
+router.register(r"users", UsersViewSet)
 
 urlpatterns = [
     path("", home, name="home-page"),
